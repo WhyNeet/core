7cec50c7b36fe99d6ac7f826cd8113d9cf050815
WhyNeet <arc.software.x@gmail.com>
2025-03-11 20:47:57 +0300

    feat(renderer): update renderer

--- crates/renderer/src/lib.rs
+++ crates/renderer/src/lib.rs
@@ -4,41 +4,65 @@ pub struct CliTreeRenderer;
 
 impl CliTreeRenderer {
     pub fn render_tree(root: TreeNode, f: &mut impl std::io::Write) -> Result<(), std::io::Error> {
-        // match root {
-        //     TreeNode::DocType(_) => (),
-        //     TreeNode::Html(nodes) | TreeNode::Body(nodes) => {
-        //         for node in nodes {
-        //             Self::render_tree(node, f)?;
-        //         }
-        //     }
-        //     TreeNode::Paragraph(nodes) => {
-        //         for node in nodes {
-        //             Self::render_tree(node, f)?;
-        //         }
-
-        //         write!(f, "\n")?;
-        //     }
-        //     TreeNode::Text(text) => {
-        //         write!(f, "{text}")?;
-        //     }
-        //     TreeNode::Link(nodes) => {
-        //         write!(f, "[link] ")?;
-
-        //         for node in nodes {
-        //             Self::render_tree(node, f)?;
-        //         }
-        //     }
-        //     TreeNode::Heading(nodes) => {
-        //         write!(f, "[heading] ")?;
-
-        //         for node in nodes {
-        //             Self::render_tree(node, f)?;
-        //         }
-
-        //         write!(f, "\n")?;
-        //     }
-        //     _ => (),
-        // };
+        match root {
+            TreeNode::Node {
+                name,
+                attributes,
+                children,
+            } => match name.as_str() {
+                "div" | "p" | "body" | "html" => {
+                    for node in children {
+                        Self::render_tree(node, f)?;
+                        write!(f, "\n")?;
+                    }
+                }
+                name if name.chars().nth(0).map(|c| c == 'h').unwrap_or(false)
+                    && name
+                        .chars()
+                        .nth(1)
+                        .map(|c| c.is_ascii_digit())
+                        .unwrap_or(false) =>
+                {
+                    write!(f, "[heading] ")?;
+                    for node in children {
+                        Self::render_tree(node, f)?;
+                        write!(f, "\n")?;
+                    }
+                }
+                "a" => {
+                    write!(
+                        f,
+                        "[link:{}] ",
+                        attributes
+                            .iter()
+                            .find(|(name, _)| name == "href")
+                            .map(|(_, href)| href.as_str())
+                            .unwrap_or("")
+                    )?;
+                    for node in children {
+                        Self::render_tree(node, f)?;
+                        write!(f, "\n")?;
+                    }
+                }
+                "head" => {
+                    for node in children {
+                        Self::render_tree(node, f)?;
+                    }
+                }
+                "meta" | "style" => (),
+                "title" => {
+                    write!(f, "Page Title: ")?;
+                    for node in children {
+                        Self::render_tree(node, f)?;
+                        write!(f, "\n")?;
+                    }
+                    write!(f, "----------\n")?;
+                }
+                "script" => (),
+                other => todo!("{other}"),
+            },
+            TreeNode::Text(text) => write!(f, "{text}")?,
+        };
 
         Ok(())
     }
