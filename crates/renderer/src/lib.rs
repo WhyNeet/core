382167de8849d23c0f0841c0848de86c7f92f1f4
WhyNeet <arc.software.x@gmail.com>
2025-03-11 21:53:58 +0300

    feat(renderer): improve page rendering

--- crates/renderer/src/lib.rs
+++ crates/renderer/src/lib.rs
@@ -1,9 +1,56 @@
+use anstyle::{Color, RgbColor, Style};
+use terminal_link::Link;
 use tree::TreeNode;
 
-pub struct CliTreeRenderer;
+#[derive(Debug, Clone, Copy)]
+pub enum TextStyling {
+    Bold,
+    Italic,
+    Underline,
+}
+
+#[derive(Debug, Clone)]
+pub struct TextStyle {
+    enclosing: Option<Box<TextStyle>>,
+    styling: TextStyling,
+}
+
+impl TextStyle {
+    pub fn get_style(&self) -> Style {
+        let mut style = Style::new();
+
+        let mut current = Some(self);
+
+        while let Some(styling) = current {
+            style = match styling.styling {
+                TextStyling::Bold => style.bold(),
+                TextStyling::Italic => style.italic(),
+                TextStyling::Underline => style.underline(),
+            };
+
+            current = styling.enclosing.as_ref().map(|s| s.as_ref());
+        }
+
+        style
+    }
+}
+
+pub struct CliTreeRenderer {
+    current_style: Option<Box<TextStyle>>,
+}
 
 impl CliTreeRenderer {
-    pub fn render_tree(root: TreeNode, f: &mut impl std::io::Write) -> Result<(), std::io::Error> {
+    pub fn new() -> Self {
+        Self {
+            current_style: None,
+        }
+    }
+
+    pub fn render_tree(
+        &mut self,
+        root: TreeNode,
+        f: &mut impl std::io::Write,
+    ) -> Result<(), std::io::Error> {
         match root {
             TreeNode::Node {
                 name,
@@ -12,19 +59,61 @@ impl CliTreeRenderer {
             } => match name.as_str() {
                 "div" | "p" | "body" | "html" | "img" | "ul" => {
                     for node in children {
-                        Self::render_tree(node, f)?;
+                        self.render_tree(node, f)?;
                     }
                     write!(f, "\n")?;
                 }
-                "small" | "i" | "b" => {
+                "small" => {
                     for node in children {
-                        Self::render_tree(node, f)?;
+                        self.render_tree(node, f)?;
                     }
+                }
+                "i" => {
+                    let enclosing = self.current_style.take();
+                    self.current_style.replace(Box::new(TextStyle {
+                        enclosing,
+                        styling: TextStyling::Italic,
+                    }));
+
+                    for node in children {
+                        self.render_tree(node, f)?;
+                    }
+
+                    let enclosing_style = self.current_style.take().unwrap().enclosing;
+                    self.current_style = enclosing_style;
+                }
+                "b" => {
+                    let enclosing = self.current_style.take();
+                    self.current_style.replace(Box::new(TextStyle {
+                        enclosing,
+                        styling: TextStyling::Bold,
+                    }));
+
+                    for node in children {
+                        self.render_tree(node, f)?;
+                    }
+
+                    let enclosing_style = self.current_style.take().unwrap().enclosing;
+                    self.current_style = enclosing_style;
+                }
+                "u" => {
+                    let enclosing = self.current_style.take();
+                    self.current_style.replace(Box::new(TextStyle {
+                        enclosing,
+                        styling: TextStyling::Underline,
+                    }));
+
+                    for node in children {
+                        self.render_tree(node, f)?;
+                    }
+
+                    let enclosing_style = self.current_style.take().unwrap().enclosing;
+                    self.current_style = enclosing_style;
                 }
                 "li" => {
                     write!(f, "  - ")?;
                     for node in children {
-                        Self::render_tree(node, f)?;
+                        self.render_tree(node, f)?;
                     }
                     write!(f, "\n")?;
                 }
@@ -35,46 +124,73 @@ impl CliTreeRenderer {
                         .map(|c| c.is_ascii_digit())
                         .unwrap_or(false) =>
                 {
-                    write!(f, "[heading] ")?;
+                    let enclosing = self.current_style.take();
+                    self.current_style.replace(Box::new(TextStyle {
+                        enclosing,
+                        styling: TextStyling::Bold,
+                    }));
+
                     for node in children {
-                        Self::render_tree(node, f)?;
-                        write!(f, "\n")?;
+                        self.render_tree(node, f)?;
                     }
+                    write!(f, "\n")?;
+
+                    let enclosing_style = self.current_style.take().unwrap().enclosing;
+                    self.current_style = enclosing_style;
                 }
                 "a" => {
-                    write!(
-                        f,
-                        "[link:{}] ",
-                        attributes
-                            .iter()
-                            .find(|(name, _)| name == "href")
-                            .map(|(_, href)| href.as_str())
-                            .unwrap_or("")
-                    )?;
+                    let href = attributes
+                        .iter()
+                        .find(|(name, _)| name == "href")
+                        .map(|(_, href)| href.as_str())
+                        .unwrap_or("");
+
+                    let mut buf = vec![];
                     for node in children {
-                        Self::render_tree(node, f)?;
+                        self.render_tree(node, &mut buf)?;
                     }
+
+                    let contents = String::from_utf8_lossy(&buf).into_owned();
+
+                    write!(f, "{}", Link::new(&contents, href))?;
                 }
                 "head" => {
                     for node in children {
-                        Self::render_tree(node, f)?;
+                        self.render_tree(node, f)?;
                     }
                 }
                 "meta" | "style" | "link" => (),
                 "br" => write!(f, "\n")?,
                 "hr" => write!(f, "\n-------------------------\n")?,
                 "title" => {
-                    write!(f, "Page Title: ")?;
+                    write!(f, "\n")?;
+
+                    let enclosing = self.current_style.take();
+                    self.current_style.replace(Box::new(TextStyle {
+                        enclosing,
+                        styling: TextStyling::Bold,
+                    }));
+
                     for node in children {
-                        Self::render_tree(node, f)?;
-                        write!(f, "\n")?;
+                        self.render_tree(node, f)?;
                     }
-                    write!(f, "----------\n")?;
+
+                    let enclosing_style = self.current_style.take().unwrap().enclosing;
+                    self.current_style = enclosing_style;
+
+                    write!(f, "\n----------\n")?;
                 }
                 "script" => (),
                 other => todo!("{other}"),
             },
-            TreeNode::Text(text) => write!(f, "{text}")?,
+            TreeNode::Text(text) => {
+                let style = self
+                    .current_style
+                    .as_ref()
+                    .map(|style| style.get_style())
+                    .unwrap_or(Style::new());
+                write!(f, "{style}{text}{style:#}")?;
+            }
         };
 
         Ok(())
